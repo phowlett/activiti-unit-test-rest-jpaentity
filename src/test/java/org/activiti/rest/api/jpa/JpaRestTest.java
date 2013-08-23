ed32d8b91c3403672c6a0d72cf32ac7fd744b269
Paul Howlett <paul@scalar.is>
2013-08-23 15:59:51 +0100

    Unit Test for getting JPA Entity Process Variable via the Task Variable Collection and Task Collection REST API calls.  Highlighting a bug in Activiti 5.13 where the call to get process variables via the Task Collection REST API fails with NPE and Internal Server Error.

---
+++ src/test/java/org/activiti/rest/api/jpa/JpaRestTest.java
@@ -0,0 +1,163 @@
+package org.activiti.rest.api.jpa;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.engine.IdentityService;
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.identity.Group;
+import org.activiti.engine.identity.User;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.activiti.engine.task.Task;
+import org.activiti.engine.test.Deployment;
+import org.activiti.rest.api.RestUrls;
+import org.activiti.rest.api.jpa.model.Message;
+import org.activiti.rest.api.jpa.repository.MessageRepository;
+import org.activiti.rest.application.ActivitiRestServicesApplication;
+import org.activiti.spring.impl.test.SpringActivitiTestCase;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.junit.Test;
+import org.restlet.Component;
+import org.restlet.data.ChallengeScheme;
+import org.restlet.data.Protocol;
+import org.restlet.data.Status;
+import org.restlet.representation.Representation;
+import org.restlet.resource.ClientResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration("classpath:activiti-context.xml")
+public class JpaRestTest extends SpringActivitiTestCase {
+	
+	@Autowired
+	private MessageRepository messageRepository;
+	
+	protected ObjectMapper objectMapper = new ObjectMapper();
+	protected Component component;
+	
+	
+	@Override
+	public void runBare() throws Throwable {
+		initializeRestServer();
+		super.runBare();
+		stopRestServer();
+	}
+	
+	@Test
+	@Deployment(resources = {"org/activiti/rest/api/jpa/jpa-process.bpmn20.xml"})
+	public void testGetJpaVariableViaTaskVariablesCollections() throws Exception {
+		
+		Message message = messageRepository.findOne(1L);
+		assertNotNull(message);
+		assertEquals("Hello World", message.getText());
+		
+		Map<String, Object> processVariables = new HashMap<String, Object>();
+	    processVariables.put("message", message);
+	    
+		ProcessInstance processInstance = processEngine.getRuntimeService().startProcessInstanceByKey("jpa-process", processVariables);
+		assertNotNull(processInstance);
+		createUsers(processEngine);
+		
+		Task task = processEngine.getTaskService().createTaskQuery().singleResult();
+		assertEquals("Activiti is awesome!", task.getName());
+		
+		 // Request all variables (no scope provides) which include global an local
+	    ClientResource client = getAuthenticatedClient(RestUrls.createRelativeResourceUrl(RestUrls.URL_TASK_VARIABLES_COLLECTION, task.getId()));
+	    Representation response = client.get();
+	    assertEquals(Status.SUCCESS_OK, client.getResponse().getStatus());
+	    
+	    JsonNode responseNode = objectMapper.readTree(response.getStream()).get(0);
+	    System.out.println(responseNode.toString());
+	    
+	    assertNotNull(responseNode);
+	    assertEquals("message", responseNode.get("name").asText());
+	    assertEquals("global", responseNode.get("scope").asText());
+	    assertEquals("serializable", responseNode.get("type").asText());
+	    assertNotNull(responseNode.get("valueUrl"));
+	    
+	    dropUsers(processEngine);
+	}
+	
+	@Test
+	@Deployment(resources = {"org/activiti/rest/api/jpa/jpa-process.bpmn20.xml"})
+	public void testGetJpaVariableViaTaskCollection() throws Exception {
+		
+		Message message = messageRepository.findOne(1L);
+		assertNotNull(message);
+		assertEquals("Hello World", message.getText());
+		
+		Map<String, Object> processVariables = new HashMap<String, Object>();
+		processVariables.put("message", message);
+	    
+		ProcessInstance processInstance = processEngine.getRuntimeService().startProcessInstanceByKey("jpa-process", processVariables);
+		assertNotNull(processInstance);
+		createUsers(processEngine);
+		
+		Task task = processEngine.getTaskService().createTaskQuery().singleResult();
+		assertEquals("Activiti is awesome!", task.getName());
+		
+		 // Request all variables (no scope provides) which include global an local
+	    ClientResource client = getAuthenticatedClient(RestUrls.createRelativeResourceUrl(RestUrls.URL_TASK_COLLECTION));
+	    client.getReference().addQueryParameter("includeProcessVariables", "true");
+	    Representation response = client.get();
+	    assertEquals(Status.SUCCESS_OK, client.getResponse().getStatus());
+	    
+	    JsonNode dataNode = objectMapper.readTree(response.getStream()).get("data").get(0);
+	    assertNotNull(dataNode);
+	    System.out.println(dataNode.toString());
+	    
+	    JsonNode variableNode = dataNode.get("variables").get(0);
+	    assertNotNull(variableNode);
+	    System.out.println(variableNode.toString());
+	    assertEquals("message", variableNode.get("name").asText());
+	    assertEquals("global", variableNode.get("scope").asText());
+
+	    assertEquals("serializable", variableNode.get("type").asText());
+	    assertNotNull(variableNode.get("valueUrl"));
+	    
+	    dropUsers(processEngine);
+	}
+	
+	protected ClientResource getAuthenticatedClient(String uri) {
+		ClientResource client = new ClientResource("http://localhost:8182/"	+ uri);
+		client.setChallengeResponse(ChallengeScheme.HTTP_BASIC, "kermit", "kermit");
+		return client;
+	}
+	
+	protected void initializeRestServer() throws Exception {
+		component = new Component();
+		// Add a new HTTP server listening on port 8182.
+		component.getServers().add(Protocol.HTTP, 8182);
+		component.getDefaultHost()
+				.attach(new ActivitiRestServicesApplication());
+		component.start();
+	}
+	
+	protected void stopRestServer() throws Exception {
+		component.stop();
+	}
+
+	protected void createUsers(ProcessEngine processEngine) {
+		IdentityService identityService = processEngine.getIdentityService();
+		User user = identityService.newUser("kermit");
+		user.setFirstName("Kermit");
+		user.setLastName("the Frog");
+		user.setPassword("kermit");
+		identityService.saveUser(user);
+
+		Group group = identityService.newGroup("admin");
+		group.setName("Administrators");
+		identityService.saveGroup(group);
+
+		identityService.createMembership(user.getId(), group.getId());
+	}
+	
+	protected void dropUsers(ProcessEngine processEngine) {
+		IdentityService identityService = processEngine.getIdentityService();
+
+		identityService.deleteUser("kermit");
+		identityService.deleteGroup("admin");
+		identityService.deleteMembership("kermit", "admin");
+	}
+
+}
